9e316d4dc29ab197e2a71e6faf992785294e9e50
xamorro <martibp2000@gmail.com>
2024-01-14 21:44:06 +0100

    phisics

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ import { gsap } from "gsap";
 import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
 import { XRControllerModelFactory } from 
                  'three/examples/jsm/webxr/XRControllerModelFactory.js';
+import CANNON from 'cannon';
+
 
 
 //Loader de models GLTF
@@ -89,30 +91,32 @@ window.addEventListener('mousemove', (event) => {
 })
 
 const material = new THREE.MeshStandardMaterial({ color: 0xf00650 });
-
-const SphereGeometry = new THREE.SphereGeometry();
-const sphere = new THREE.Mesh(SphereGeometry, material);
-sphere.position.set(-10, 20, 30);
-sphere.scale.set(10, 10, 10);
-sphere.castShadow = true;
-sphere.receiveShadow = true;
-//scene.add(sphere);
+const material2 = new THREE.MeshStandardMaterial({ color: 0xd0d0d0 });
 
 const PlaneGeometry = new THREE.PlaneGeometry();
 const plane = new THREE.Mesh(PlaneGeometry, material);
-plane.position.set(-10, 10, 30);
+plane.position.set(-10, -30, 30);
 plane.scale.set(100, 100, 100);
-plane.rotation.set(5, 0, 0);
+plane.rotation.set(4.7, 0, 0);
 plane.castShadow = true;
 plane.receiveShadow = true;
-//scene.add(plane);
+scene.add(plane);
+
+const SphereGeometry = new THREE.SphereGeometry();
+const sphere = new THREE.Mesh(SphereGeometry, material);
+sphere.position.set(0, 10, 0);
+sphere.scale.set(1, 1, 1);
+sphere.castShadow = true;
+sphere.receiveShadow = true;
+scene.add(sphere);
+
 
 
 //---------------------CAMERA CONTROL----------------------
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-camera.position.set(0, 30, 130)
-camera.rotation.set(0, 30, 2)
-camera.lookAt(new THREE.Vector3(0, 0, 0))
+camera.position.set(0, 5, 80)
+//camera.rotation.set(0, 30, 2)
+//camera.lookAt(new THREE.Vector3(0, 0, 0))
 
 const renderer = new THREE.WebGLRenderer()
 //Activar ombres
@@ -183,36 +187,122 @@ controller2.addEventListener( 'disconnected', function () {
   this.remove( this.children[ 0 ] );
 } );
 
-
 scene.add(controller1)
 scene.add(controller2)
+
+const controllerModelFactory = new XRControllerModelFactory();
+const controllerGrip1 = renderer.xr.getControllerGrip( 0 );
+controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
+scene.add( controllerGrip1 );
+
+const controllerGrip2 = renderer.xr.getControllerGrip( 1 );
+controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
+scene.add( controllerGrip2 );
+
+function buildController( data ) {
+  let geometry, material;
+  switch ( data.targetRayMode ) {
+
+      case 'tracked-pointer':
+          geometry = new THREE.BufferGeometry();
+          geometry.setAttribute( 'position', 
+                         new THREE.Float32BufferAttribute( [ 0, 0, 0, 0, 0, - 1 ], 3 ) )
+          geometry.setAttribute( 'color', 
+                         new THREE.Float32BufferAttribute( [ 0.5, 0.5, 0.5, 0, 0, 0 ], 3 ) )
+          material = new THREE.LineBasicMaterial( 
+                        { 
+                          vertexColors: true, 
+                          blending: THREE.AdditiveBlending
+                        }
+           )
+          return new THREE.Line( geometry, material )
+          case 'gaze':
+
+          geometry = new THREE.RingGeometry( 0.02, 0.04, 32 ).translate(0,0, - 1)
+          material = new THREE.MeshBasicMaterial( { 
+                      opacity: 0.5, 
+                      transparent: true
+           } )
+          return new THREE.Mesh( geometry, material )
+  }
+}
+
+function handleController( controller ) {
+  if ( controller.userData.isSelecting ) {
+      // Acció en prémer el botó de Select del controlador
+      console.log('Selecting')
+  }
+}
+
+// gsap.to(sphere.position, {
+//   duration: 1,
+//   x: 0, //DRETA ESQUERRA
+//   y: 30, //ADALT ABAIX
+//   z: 0,
+//   yoyo: true,
+//   repeat: -1, // repetir indef
+//   ease: "power1.inOut", // tipus de transcisió
+// });
+
+//------------MON-----------------------//
+const world = new CANNON.World()
+world.gravity.set(0, - 9.82, 0)
+
+const sphereShape = new CANNON.Sphere(1)
+
+const sphereBody = new CANNON.Body({
+  mass: 1,
+  position: new CANNON.Vec3(0, 0, 0),
+  shape: sphereShape
+})
+world.addBody(sphereBody)
+
+// phisic plane MON
+const floorShape = new CANNON.Plane()
+const floorBody = new CANNON.Body({
+  mass: 0,
+  position: new CANNON.Vec3(0, -1, 0),
+  shape: floorShape
+})
+world.addBody(floorBody)
+
+const clock = new THREE.Clock()
+let oldElapsedTime = 0
+
+
+
+
+
+
+
+
 
 
 
 const guitar = new THREE.Object3D()
-ImportGLTF("Models/bass_guitar_low_poly_freebie.glb", guitar, new THREE.Vector3(15, 15, 15));
-guitar.position.set(-55, 14, -25);
+ImportGLTF("Models/bass_guitar_low_poly_freebie.glb", guitar, new THREE.Vector3(5, 5, 5));
+guitar.position.set(-18, 3, -25);
 scene.add(guitar);
 
 const piano = new THREE.Object3D()
-ImportGLTF("Models/old_piano.glb", piano, new THREE.Vector3(0.2, 0.2, 0.2));
-piano.position.set(-15, 4, -25);
+ImportGLTF("Models/old_piano.glb", piano, new THREE.Vector3(0.05, 0.05, 0.05));
+piano.position.set(-5, 0, -25);
 scene.add(piano);
 
 const violin = new THREE.Object3D()
-ImportGLTF("Models/stylized_violin.glb", violin, new THREE.Vector3(30, 30, 30), new THREE.Vector3(0, 35, 0));
-violin.position.set(15, 13, -25);
+ImportGLTF("Models/stylized_violin.glb", violin, new THREE.Vector3(10, 10, 10), new THREE.Vector3(0, 35, 0));
+violin.position.set(5, 1.5, -25);
 scene.add(violin);
 
 const organ = new THREE.Object3D()
-ImportGLTF("Models/pipe_organ_espresso_machine.glb", organ, new THREE.Vector3(15, 15, 15));
-organ.position.set(45, 5, -25);
+ImportGLTF("Models/pipe_organ_espresso_machine.glb", organ, new THREE.Vector3(5, 5, 5));
+organ.position.set(15, -1, -25);
 scene.add(organ);
 
 const theatre = new THREE.Object3D()
 ImportGLTF("Models/theatre_cheap_template.glb", theatre, new THREE.Vector3(2, 2, 2), new THREE.Vector3(0, 3.15, 0));
 theatre.position.set(0, -5.5, -25);
-scene.add(theatre);
+//scene.add(theatre);
 
 
 
@@ -430,7 +520,21 @@ function tick(time) {
 
     // ...
   )
+  handleController( controller1 );
+  handleController( controller2 );
 
+
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - oldElapsedTime
+  oldElapsedTime = elapsedTime
+
+  // Update physics
+  world.step(1 / 60, deltaTime, 3)
+  console.log(sphereBody.position.y)
+  sphere.position.copy(sphereBody.position)
+  plane.position.copy(floorBody.position)
+
+  floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), Math.PI * 0.5)
 
   // Update controls
   //controls.update()
@@ -445,75 +549,6 @@ function tick(time) {
     point.element.style.transform = `translateX(${translateX}px)translateY(${translateY}px)`
 
   }
-  //------------- Anima objectes abaix cap a dalt
-  // const elapsedTime = clock.getElapsedTime()
-
-  // sphere.position.y = Math.sin(elapsedTime * 4.3) * 5.5 + 15
-  // sphere2.position.y = Math.sin(elapsedTime * 0.8) * 5.5 + 15
-  // sphere3.position.y = Math.sin(elapsedTime * 1.4) * 1.5 + 15
-  //------------- ---------------------------------
-
-  //   rayDirection.normalize()
-
-  //----------------RAYCASTER LINIA -----------
-  // raycaster.set(rayOrigin, rayDirection)
-  //------------------------------------------------
-
-  // if(sceneReady) {
-  //   for(const point of points) {
-
-  //   const screenPosition = point.position.clone()
-  //   screenPosition.project(camera)
-
-  //   //raycaster.setFromCamera(screenPosition, camera)
-  //   const intersects = raycaster.intersectObjects(scene.children, true)
-
-  //   if(intersects.length === 0) {
-  //     point.element.classList.add('visible')
-  //     }
-  //     else {
-  //       const intersectionDistance = intersects[0].distance
-  //       const pointDistance = point.position.distanceTo(camera.position)
-  //       if(intersectionDistance < pointDistance) {
-  //         point.element.classList.remove('visible')
-  //       }
-  //       else {
-  //       point.element.classList.add('visible')
-  //       }
-  //     }
-  //   }
-  // }
-
-
-
-  // const objectsToTest = [sphere, sphere2, sphere3,]
-  // const ObjecteSol = [sphere]
-  // //   //Feim q es raycast interectiu amb 1 sol objecte o varis
-  // const intersects = raycaster.intersectObjects(objectsToTest)
-  // //   console.log(intersects)
-
-  // //----------------RAYCASTER MOUSE -----------
-  // raycaster.setFromCamera(mouse, camera)
-  // //------------------------------------------------
-
-  // //   //const objectsToTest = raycaster.intersectObjects([sphere, sphere2, sphere3])
-  // //   //console.log(intersects)
-
-
-
-  // for (const intersect of intersects) {
-  //   console.log("Canvi Color DAMUNT")
-  //   intersect.object.material.color.set('#0000ff')
-  // }
-
-  // for(const object of objectsToTest) {
-  //   if(!intersects.find(intersect => intersect.object === object)) {
-  //     object.material.color.set('#ff0000')
-  //   }
-  // }
-
- 
-
 
   renderer.render(scene, camera)
   renderer.setAnimationLoop(tick);
